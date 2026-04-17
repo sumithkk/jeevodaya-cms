d4c88eec8af7f36288fcd35d2ba9c7ccf77480b0
Sumith kk <sumithkk2007@gmail.com>
2026-04-17 18:58:59 +0530

    Add Sanity deployment appId config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,10 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
+    appId: 'vebolxy644rbl5a1u9ksc67g',
     autoUpdates: true,
   }
 })
